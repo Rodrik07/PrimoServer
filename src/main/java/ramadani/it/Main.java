9d56e9e43f7d4125c0b4f1ccb2c0e3e8cb301d69
lai2-24 <lai2-24@itismeucci.com>
2025-10-06 09:28:46 +0200

    Primo Server Creato

--- src/main/java/ramadani/it/Main.java
+++ src/main/java/ramadani/it/Main.java
@@ -1,7 +1,16 @@
 package ramadani.it;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        ServerSocket mioServerSocket = new ServerSocket(3000);
+        //il mio main quando arriva qui si ferma e quello sotto non viene eseguito fino a quando qualcuno si collega e mi restituisce un socket
+        Socket mioSocket =  mioServerSocket.accept();
+        System.out.println("Un CLIENT si e' connesso");
+        
     }
 }
